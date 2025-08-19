345a763dd1cc7a09fc6842adb67f3b85127be9a4
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-19 19:30:59 +0300

    corrected the method of cloning column, simplified the function call and reduced the count of methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,9 @@
 'use strict';
 
-// write your code here
-const thead = document.querySelector('thead');
-const theadRows = Array.from(thead.children);
-const tbody = document.querySelector('tbody');
-const people = Array.from(tbody.children);
-const tfoot = document.querySelector('tfoot');
-const tfootRows = Array.from(tfoot.children);
+const rows = document.querySelectorAll('thead tr, tbody tr, tfoot tr');
 
 // duplicate a second column to a pre-last position
-function duplicateSecondColumn(list, elementToCreate) {
+function duplicateSecondColumn(list) {
   for (const char of list) {
     if (char.children.length === 0) {
       continue;
@@ -17,14 +11,10 @@ function duplicateSecondColumn(list, elementToCreate) {
 
     const position = char.children[1];
     const preLast = char.children[char.children.length - 1];
-    const clone = document.createElement(elementToCreate);
-
-    clone.textContent = position.textContent;
+    const clone = position.cloneNode(true);
 
     preLast.before(clone);
   }
 }
 
-duplicateSecondColumn(theadRows, 'th');
-duplicateSecondColumn(people, 'td');
-duplicateSecondColumn(tfootRows, 'th');
+duplicateSecondColumn(rows);
